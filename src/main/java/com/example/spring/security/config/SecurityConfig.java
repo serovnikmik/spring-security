5aada11e11b6d4a8903d16d3a9e852e585408b88
What Git <serovnikmik@gmail.com>
2022-10-17 18:23:53 +0300

    some changes, creating user, admin, saving them to db

--- src/main/java/com/example/spring/security/config/SecurityConfig.java
+++ src/main/java/com/example/spring/security/config/SecurityConfig.java
@@ -1,8 +1,16 @@
 package com.example.spring.security.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -15,6 +23,48 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .and()
                 .logout().logoutSuccessUrl("/");
+    }
+
+//    @Bean
+//    public UserDetailsService users(){
+//        UserDetails user = User.builder()
+//                .username("user")
+//                .password("{bcrypt}$2a$12$qxFUGFjx3ZZldbYJcfvhUeGs6oZIMzFjpBEBzo4uFUKcyihTmLZF2")
+//                .roles("USER")
+//                .build();
+//        UserDetails admin = User.builder()
+//                .username("admin")
+//                .password("{bcrypt}$2a$12$qxFUGFjx3ZZldbYJcfvhUeGs6oZIMzFjpBEBzo4uFUKcyihTmLZF2")
+//                .roles("ADMIN", "USER")
+//                .build();
+//        return new InMemoryUserDetailsManager(user, admin);
+//    }
+
+    //jdbc authentication
+    @Bean
+    public JdbcUserDetailsManager users(DataSource dataSource){
+        UserDetails user = User.builder()
+                .username("user")
+                .password("{bcrypt}$2a$12$qxFUGFjx3ZZldbYJcfvhUeGs6oZIMzFjpBEBzo4uFUKcyihTmLZF2")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{bcrypt}$2a$12$qxFUGFjx3ZZldbYJcfvhUeGs6oZIMzFjpBEBzo4uFUKcyihTmLZF2")
+                .roles("ADMIN", "USER")
+                .build();
+
+        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+        if (users.userExists(user.getUsername())){
+            users.deleteUser(user.getUsername());
+        }
+        if (users.userExists(admin.getUsername())){
+            users.deleteUser(admin.getUsername());
+        }
+        users.createUser(user);
+        users.createUser(admin);
+
+        return users;
     }
 
 }
--- src/main/java/com/example/spring/security/controller/MainController.java
+++ src/main/java/com/example/spring/security/controller/MainController.java
@@ -4,6 +4,8 @@ package com.example.spring.security.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
 public class MainController {
 
@@ -13,8 +15,8 @@ public class MainController {
     }
 
     @GetMapping("/authenticated")
-    public String pageForAuthenticatedUsers(){
-        return "secured part of the web site";
+    public String pageForAuthenticatedUsers(Principal principal){
+        return "secured part of the web site: " + principal.getName();
     }
 
 }
